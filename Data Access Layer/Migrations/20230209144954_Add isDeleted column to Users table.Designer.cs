07aba2202948299484fa555bbd883a3bfb271bbf
Oleksandr Myroshnychenko <Oleksandr_Myroshnychenko@epam.com>
2023-02-09 16:51:04 +0200

    Added Users and UsersAnswers models and tables to database. Added repository and UoW for Users. Added migrations

--- Data Access Layer/Migrations/20230209144954_Add isDeleted column to Users table.Designer.cs
+++ Data Access Layer/Migrations/20230209144954_Add isDeleted column to Users table.Designer.cs
@@ -4,14 +4,16 @@ using Data_Access_Layer.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Data_Access_Layer.Migrations
 {
     [DbContext(typeof(MssqlContext))]
-    partial class MSSQLContextModelSnapshot : ModelSnapshot
+    [Migration("20230209144954_Add isDeleted column to Users table")]
+    partial class AddisDeletedcolumntoUserstable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Data Access Layer/Models/UserAnswers.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Data_Access_Layer.Models
+{
+    public class UserAnswer
+    {
+        public Guid Id { get; set; }
+
+        public User User { get; set; }
+
+        public Question Question { get; set; }
+
+        public string Answer { get; set; }
+    }
+}
