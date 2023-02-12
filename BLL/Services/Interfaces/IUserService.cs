061b874e2e04386152d7a4f4113ad291b21a9f7e
Oleksandr Myroshnychenko <Oleksandr_Myroshnychenko@epam.com>
2023-02-12 15:14:39 +0200

    Fix project to be able publish on Azure server

---
+++ BLL/Services/Interfaces/IUserService.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using System;
+using Business_Logic_Layer.Models;
+
+namespace Business_Logic_Layer.Services.Interfaces
+{
+    public interface IUserService
+    {
+        public Task AddAsync(UserModel model);
+
+        public Task AddAnswers(UserModel model);
+
+        public Task UpdateAsync(UserModel model);
+
+        public Task UpdateAnswers(UserModel model);
+
+        public Task DeletePermanentlyAsync(UserModel model);
+
+        public Task DeleteTemporarilyAsync(Guid id);
+
+        public Task<UserModel> GetByIdAsync(Guid id);
+
+        public Task<UserModel> GetByIdWithDetailsAsync(Guid id);
+
+        public Task<List<UserModel>> GetAllAsync(bool includeDeleted = false);
+
+        public Task<List<UserModel>> GetAllWithDetailsAsync(bool includeDeleted = false);
+
+        public Task<UserModel> GetOrCreateUserByIdAsync(Guid id);
+    }
+}
---
+++ DAL/Repositories/Interfaces/IUserRepository.cs
@@ -0,0 +1,30 @@
+﻿using Data_Access_Layer.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System;
+
+namespace Data_Access_Layer.Repositories.Interfaces
+{
+    public interface IUserRepository
+    {
+        public Task<User> AddAsync(User entity);
+
+        public void Update(User entity);
+
+        public void UpdateAnswers(IEnumerable<UserAnswer> answers);
+
+        public Task AddAnswersAsync(IEnumerable<UserAnswer> answers);
+
+        public void DeletePermanently(User entity);
+
+        public Task DeleteTemporarilyAsync(Guid id);
+
+        public Task<User> GetByIdAsync(Guid id);
+
+        public Task<User> GetByIdWithDetailsAsync(Guid id);
+
+        public Task<List<User>> GetAllAsync(bool includeDeleted = false);
+
+        public Task<List<User>> GetAllWithDetailsAsync(bool includeDeleted = false);
+    }
+}
