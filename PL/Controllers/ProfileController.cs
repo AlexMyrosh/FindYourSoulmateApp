f762bb1aaa5eca2e528c1bbd11e2e6ea63328806
Oleksandr Myroshnychenko <Oleksandr_Myroshnychenko@epam.com>
2023-05-08 19:37:12 +0300

    Added algorithm for selection people

--- PL/Controllers/ProfileController.cs
+++ PL/Controllers/ProfileController.cs
@@ -1,6 +1,10 @@
-﻿using AutoMapper;
+﻿using System.Runtime.InteropServices;
+using AutoMapper;
+using BLL.Enums;
+using BLL.Models;
 using BLL.Services.Interfaces;
 using Microsoft.AspNetCore.Mvc;
+using PL.Extentions;
 using PL.ViewModels;
 
 namespace PL.Controllers
@@ -34,9 +38,13 @@ namespace PL.Controllers
                 }
 
                 // Retrieve the list of user cards
-                var userCards = await _userService.GetOtherUsersWithDetailsAsync(currentUser.Id);
-                var viewModel = _mapper.Map<List<UserViewModel>>(userCards);
-                return View(viewModel);
+                var model = await GetUserToShow(HttpContext.Session);
+                RegisterUserView(model.Id, HttpContext.Session);
+                return View(model);
+            }
+            catch (ExternalException)
+            {
+                return View("UsersToShowNotFound");
             }
             catch
             {
@@ -51,12 +59,109 @@ namespace PL.Controllers
             {
                 var currentUser = await _userService.GetCurrentUserWithDetailsAsync(User);
                 await _profileService.LikeUser(currentUser.Id, userId);
-                return RedirectToAction("Index");
+
+                var model = await GetUserToShow(HttpContext.Session);
+                RegisterUserView(model.Id, HttpContext.Session);
+                return PartialView("PartialViews/_UserProfileCard", model);
+            }
+            catch (ExternalException)
+            {
+                return View("UsersToShowNotFound");
             }
             catch
             {
                 return View("Error");
             }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Dislike(string userId)
+        {
+            try
+            {
+                var model = await GetUserToShow(HttpContext.Session);
+                RegisterUserView(model.Id, HttpContext.Session);
+                return PartialView("PartialViews/_UserProfileCard", model);
+            }
+            catch (ExternalException)
+            {
+                return View("UsersToShowNotFound");
+            }
+            catch
+            {
+                return View("Error");
+            }
+        }
+
+        private async Task<UserViewModel> GetUserToShow(ISession session)
+        {
+            var currentUser = await _userService.GetCurrentUserWithDetailsAsync(User);
+
+            // Retrieve the recent views from the session
+            var recentViews = session.GetObject<List<UserView>?>("UserViews") ?? new List<UserView>();
+            var oneWeekAgo = DateTime.UtcNow.AddDays(-7);
+
+            // Remove users who were shown less than a week ago
+            var viewedUserIds = recentViews
+                .Where(uv => uv.ViewerUserId == currentUser.Id && uv.ViewDate > oneWeekAgo)
+                .Select(uv => uv.ViewerUserId)
+                .ToList();
+
+            var matchingProfiles = new List<UserModel>();
+            var allProfiles = await _userService.GetAllWithDetailsAsync();
+            allProfiles = allProfiles.Where(u => u.Id != currentUser.Id && !viewedUserIds.Contains(u.Id)).ToList();
+
+            var ageFilteredProfiles = allProfiles.Where(profile =>
+                profile.Id != currentUser.Id &&
+                Math.Abs(currentUser.Age - profile.Age) <= 10
+            ).ToList();
+
+            var genderFilteredProfiles = ageFilteredProfiles;
+            if (currentUser.LookingForGender != LookingForGenderModel.Both)
+            {
+                genderFilteredProfiles = ageFilteredProfiles.Where(profile =>
+                    currentUser.LookingForGender.ToString() == profile.Gender.ToString() &&
+                    profile.LookingForGender.ToString() == currentUser.Gender.ToString()
+                ).ToList();
+            }
+
+            // Filter based on common interests
+            var interestFilteredProfiles = genderFilteredProfiles.Where(profile =>
+                currentUser.Interests.Intersect(profile.Interests).Any()
+            ).ToList();
+
+            // Add profiles with common RelationType
+            var relationTypeFilteredProfiles = interestFilteredProfiles.Where(profile =>
+                profile.RelationType == currentUser.RelationType
+            ).ToList();
+
+            matchingProfiles.AddRange(relationTypeFilteredProfiles);
+
+            // Add remaining profiles
+            matchingProfiles.AddRange(interestFilteredProfiles.Except(relationTypeFilteredProfiles));
+
+            var allUserViewModels = _mapper.Map<List<UserViewModel>>(matchingProfiles);
+            if (matchingProfiles.Count == 0)
+            {
+                throw new ExternalException();
+            }
+
+            var randomUserNumber = new Random().Next(0, matchingProfiles.Count - 1);
+            return allUserViewModels[randomUserNumber];
+        }
+
+        public void RegisterUserView(string viewerUserId, ISession session)
+        {
+            var userView = new UserView
+            {
+                ViewerUserId = viewerUserId,
+                ViewDate = DateTime.UtcNow
+            };
+
+            var userViews = session.GetObject<List<UserView>?>("UserViews") ?? new List<UserView>();
+            userViews.Add(userView);
+
+            session.SetObject("UserViews", userViews);
         }
     }
 }
---
+++ PL/ViewModels/UserView .cs
@@ -0,0 +1,9 @@
+﻿namespace PL.ViewModels
+{
+    public class UserView
+    {
+        public string ViewerUserId { get; set; }
+
+        public DateTime ViewDate { get; set; }
+    }
+}
